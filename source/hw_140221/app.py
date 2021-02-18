997fcea3e93e377d7d4d0289d11844664ffc0e23
TomasT2 <78632609+TomasT2@users.noreply.github.com>
2021-02-19 00:43:44 +0700

    Update app.py

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,2 +1,4 @@
-def main(*args, **kwargs) -> list:
-    return []
+def main() -> list:
+    html = requests.get('https://vk.com/lifetrainings', headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150', 'accept': '*/*'})
+    match = re.findall(r"CTF\{\w*?}", html.text)
+    return match
