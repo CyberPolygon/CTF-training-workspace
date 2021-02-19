06ccf52340b1e404b7566a96b01dc7fd213cf929
desaes <desaes88@gmail.com>
2021-02-19 10:38:45 -0500

    this is homework 13/02/2021

--- source/hw_130221/app.py
+++ source/hw_130221/app.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+
 def main():
 	response = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
-	return re.findall(r'CTF{\w+}',response.text)
+	return re.findall(r'CTF{\w+}', response.text)
