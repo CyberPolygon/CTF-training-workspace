76a94f2c7e5ab098aaf03e31a3157e56bd9d4448
Semikola2 <79004180+Semikola2@users.noreply.github.com>
2021-02-20 00:29:05 +0700

    блядская загрузка папки

---
+++ source/hw_130221/app.py
@@ -0,0 +1,18 @@
+import requests
+import re
+
+def get_flags() -> list:
+    request = requests.get('https://vk.com/wall-171787766_172')
+
+    searchFlags = re.findall('CTF\{[a-z,A-Z,0-9,а-я,А-Я]{1,}\}', request.text)
+    if not searchFlags:
+        print('Флаги не обнаружены')
+    else:
+        return searchFlags
+    return []
+
+#if __name__ == '__main__':
+#    print(get_flags())
+
+def main() -> list:
+    return get_flags()
