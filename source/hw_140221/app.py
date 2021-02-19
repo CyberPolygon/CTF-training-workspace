68a6216d23beb2aba334269bf6d176c0fce68cad
Semikola2 <79004180+Semikola2@users.noreply.github.com>
2021-02-19 23:30:25 +0700

    Add files via upload

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,2 +1,15 @@
-def main(*args, **kwargs) -> list:
+import requests
+import re
+
+def get_flags() -> list:
+    request = requests.get('https://vk.com/wall-171787766_172')
+
+    searchFlags = re.findall('[C,С][T,Т]F\{[a-z,A-Z,0-9,а-я,А-Я]{1,}\}', request.text)
+    if not searchFlags:
+        print('Флаги не обнаружены')
+    else:
+        return searchFlags
     return []
+
+if __name__ == '__main__':
+    print(get_flags())
