d65784d66b5e948e3b94d89823f5dc59b6344f9f
Domalega <vladimir.domalega@gmail.com>
2021-02-19 20:13:13 +0700

    Новое регулярное выражение

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -2,8 +2,8 @@ import requests
 import re
 
 def main() -> list:
-    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172', headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150', 'accept': '*/*'})
-    ctfflags = re.findall( r'CTF\[{]\w+[}]', requestvk.text)
+    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172', headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150', 'accept': '*/*'})
+    ctfflags = list(set(re.findall(r'CTF\{\w*?}', requestvk.text)))
 
     file = open('CTF_1.txt', "w")
     for element in ctfflags:
