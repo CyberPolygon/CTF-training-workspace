b3cf8b86d813482247abb94a764245778e45afb9
leonid <leonid543281@gmail.com>
2021-02-19 23:10:36 +0700

    изменил

--- source/hw_130221/app.py
+++ source/hw_130221/app.py
@@ -1,10 +1,10 @@
 import requests
 import re
 
-def main(): 
+def main():
     URL = "https://vk.com/lifetrainings?w=wall-171787766_172"
     responce = requests.get(URL).text
 
-    x = r"CTF{\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S\S}"
-    a = re.findall(x, responce)
-    return a
+    x = r"CTF{\w*}"
+    return re.findall(x, responce)
+print(main())
