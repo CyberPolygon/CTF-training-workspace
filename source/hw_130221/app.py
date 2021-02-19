d576e151ef5b1bdf6c89001e479d2a81d6aeefe4
Tomast2 <tima2412@outlook.com>
2021-02-19 14:48:25 +0700

    Updated app.py

--- source/hw_130221/app.py
+++ source/hw_130221/app.py
@@ -2,3 +2,5 @@ def main() -> list:
     html = requests.get('https://vk.com/wall-171787766_172', headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150', 'accept': '*/*'})
     match = list(set(re.findall(r"CTF\{\w*?}", html.text)))
     return match
+
+print('test')
