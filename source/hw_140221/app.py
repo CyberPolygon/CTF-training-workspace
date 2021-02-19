00e57d5c5d57708ca6247bf8925ce5c52a0b0d1f
Domalega <vladimir.domalega@gmail.com>
2021-02-19 20:05:01 +0700

    Добавлена защита от проверки на бота

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,8 +1,8 @@
-dimport requests
+import requests
 import re
 
 def main() -> list:
-    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
+    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172', headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150', 'accept': '*/*'})
     ctfflags = re.findall( r'CTF\[{]\w+[}]', requestvk.text)
 
     file = open('CTF_1.txt', "w")
