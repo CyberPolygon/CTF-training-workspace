85b230b52c62901ca7bb375cfc32a5ce71d0d851
Anton-Duduinsky <dudinsky.anton@yandex.ru>
2021-02-19 06:51:54 -0500

    Implement task 140221 * Add the main () function, which includes an abstract implementation of the task.

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,2 +1,16 @@
-def main(*args, **kwargs) -> list:
+import requests
+import re
+
+def main() -> list:
+
+    r = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
+
+    CTFka = re.findall( '\w\w\w[{]\w+[}]', r.text)
+
+    File = open('CTF.txt', "w", encoding="utf-8")
+    for element in CTFka:
+         File.write(element)
+         File.write('\n')
+    File.close()
     return []
+main()
