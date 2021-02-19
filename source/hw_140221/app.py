a97a27387609d1517f799966e9517f2453c5e11a
Anton-Duduinsky <dudinsky.anton@yandex.ru>
2021-02-19 07:59:16 -0500

    fix issue

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -2,15 +2,14 @@ import requests
 import re
 
 def main() -> list:
-
     r = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
 
-    CTFka = re.findall( '\w\w\w[{]\w+[}]', r.text)
+    CTFka = re.findall( '[A-Z]\w\w[{]\w+[}]', r.text)
 
     File = open('CTF.txt', "w", encoding="utf-8")
     for element in CTFka:
          File.write(element)
          File.write('\n')
     File.close()
-    return []
+    return CTFka
 main()
