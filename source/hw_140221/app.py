7b7af9b4e26fac6f1c6263a64ce089fba682d8f3
Domalega <vladimir.domalega@gmail.com>
2021-02-19 19:59:19 +0700

    Изменения в регулярном вырарегулярном выражениижении

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,2 +1,15 @@
-def main(*args, **kwargs) -> list:
-    return []
+dimport requests
+import re
+
+def main() -> list:
+    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
+    ctfflags = re.findall( r'CTF\[{]\w+[}]', requestvk.text)
+
+    file = open('CTF_1.txt', "w")
+    for element in ctfflags:
+        file.write(element)
+        file.write('\n')
+    file.close()
+    return ctfflags
+
+main()
