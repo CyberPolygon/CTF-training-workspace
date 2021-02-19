7cbe272e90745ea09d18a25dfef66df3ae94b051
Domalega <vladimir.domalega@gmail.com>
2021-02-19 19:50:55 +0700

    Реализовал задачу 140221 * Реализована оснаваная логика программы.

--- source/hw_140221/app.py
+++ source/hw_140221/app.py
@@ -1,15 +1,2 @@
-import requests
-import re
-
-def main() -> list:
-    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
-    ctfflags = re.findall( '\w\w\w[{]\w+[}]', requestvk.text)
-
-    file = open('CTF_1.txt', "w")
-    for element in ctfflags:
-        file.write(element)
-        file.write('\n')
-    file.close()
-    return ctfflags
-
-main()
+def main(*args, **kwargs) -> list:
+    return []
