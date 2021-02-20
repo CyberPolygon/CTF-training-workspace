cd4e70b40fb0f73f5f578054755caecd231abc37
headst1337 <ivan.panov.01@gmail.com>
2021-02-20 13:33:17 +0700

    Финалочка

--- source/hw_130221/app.py
+++ source/hw_130221/app.py
@@ -1,6 +1,9 @@
 import requests
 import re
 
+
 def main(*args, **kwargs) -> list:
     r = requests.get('https://vk.com/wall-171787766_172')
-    return [re.findall(r'CTF{(\w+)', r.text)]
+    return re.findall(r'CTF{\w+}', r.text)
+
+
